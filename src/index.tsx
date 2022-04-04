20ed49196b50a5da7a4304a63f602fa1e96ee7fd
dylan <hcj_of@outlook.com>
2022-04-04 14:07:39 +0800

    ci(redux): 增加redux 和redux-thunk

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,36 @@
+/*
+ * @Author: hanchengji
+ * @Date: 2022-04-03
+ * @LastEditors: hanchengji
+ * @LastEditTime: 2022-04-04
+ * @FilePath: /react-demo/src/index.tsx
+ * @Description:
+ *
+ * Copyright (c) 2022 by hanchengji/跨越新科技, All Rights Reserved.
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+
+// Create a root.
+
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
